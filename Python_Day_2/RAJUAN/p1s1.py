338c4816ee9948d524b0d0061b35c2268e84c657
Al-Arashad Ahmed Rajuan <71528293+aaarajuan@users.noreply.github.com>
2022-10-15 15:18:38 +0600

    Create p1s1.py

---
+++ Python_Day_2/RAJUAN/p1s1.py
@@ -0,0 +1,4 @@
+a = int(input('Enter the number: ')) # get user input.
+while (a <= 15): # assigning a condition the number mustn't be greater than 15.
+    print(a) # printing the result
+    a += 1 # usning operator '+=' it's a short form of a = a + 1.
